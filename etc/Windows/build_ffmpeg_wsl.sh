66303a72a625b0ff53f6a58b3f6694f0d7fc1d17
Darby Johnston <darbyjohnston@yahoo.com>
2021-04-29 09:43:22 -0700

    Fix script

--- etc/Windows/build_ffmpeg_wsl.sh
+++ etc/Windows/build_ffmpeg_wsl.sh
@@ -1,24 +1,24 @@
 #!/bin/sh
 
-#wget https://ffmpeg.org/releases/ffmpeg-4.4.tar.bz2
-#tar xvf ffmpeg-4.4.tar.bz2
+wget https://ffmpeg.org/releases/ffmpeg-4.4.tar.bz2
+tar xvf ffmpeg-4.4.tar.bz2
 cd ffmpeg-4.4
-#./configure \
-#    --disable-programs \
-#    --disable-doc \
-#    --disable-bzlib \
-#    --disable-iconv \
-#    --disable-lzma \
-#    --disable-vaapi \
-#    --enable-pic \
-#    --arch=x86_64 \
-#    --cross-prefix=x86_64-w64-mingw32- \
-#    --target-os=mingw32 \
-#    --enable-cross-compile \
-#    --prefix=$1 \
-#    --enable-shared
-#make -j
-#make -j install
+./configure \
+    --disable-programs \
+    --disable-doc \
+    --disable-bzlib \
+    --disable-iconv \
+    --disable-lzma \
+    --disable-vaapi \
+    --enable-pic \
+    --arch=x86_64 \
+    --cross-prefix=x86_64-w64-mingw32- \
+    --target-os=mingw32 \
+    --enable-cross-compile \
+    --prefix=$1 \
+    --enable-shared
+make -j
+make -j install
 
 # Currently there is a bug where the .lib files are installed in the bin
 # directory instead of the lib directory
