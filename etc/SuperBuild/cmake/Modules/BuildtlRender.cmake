03342c414c76d98ac21f1bd7b2d95e9a6d9bd724
Darby Johnston <darbyjohnston@yahoo.com>
2021-08-19 12:46:34 -0700

    Build fixes

--- etc/SuperBuild/cmake/Modules/BuildtlRender.cmake
+++ etc/SuperBuild/cmake/Modules/BuildtlRender.cmake
@@ -6,7 +6,8 @@ set(tlRender_ARGS
     -DTLR_ENABLE_PYTHON=${TLR_ENABLE_PYTHON}
     -DTLR_BUILD_GL=${TLR_BUILD_GL}
     -DTLR_BUILD_QT=${TLR_BUILD_QT}
-    -DTLR_BUILD_TESTS=${TLR_BUILD_EXAMPLES}
+    -DTLR_BUILD_APPS=${TLR_BUILD_APPS}
+    -DTLR_BUILD_EXAMPLES=${TLR_BUILD_EXAMPLES}
     -DTLR_BUILD_TESTS=${TLR_BUILD_TESTS})
 
 set(tlRender_DEPENDS OTIO OCIO FSeq)
@@ -23,8 +24,8 @@ if(TLR_BUILD_FFmpeg)
     list(APPEND tlRender_DEPENDS FFmpeg)
 endif()
 if(TLR_BUILD_GL)
-    list(APPEND tlRender_DEPENDS GLFW glad FreeType)
-    if(TLR_BUILD_EXAMPLES)
+    list(APPEND tlRender_DEPENDS glad FreeType)
+    if(TLR_BUILD_APPS OR TLR_BUILD_EXAMPLES)
         list(PREPEND tlRender_DEPENDS GLFW)
     endif()
 endif()
