a2a0b47d160735541c2dcec75fb3b563725182df
Darby Johnston <darbyjohnston@yahoo.com>
2021-08-19 13:47:10 -0700

    Don't install docs, examples, etc.

--- etc/SuperBuild/cmake/Modules/Buildlibsamplerate.cmake
+++ etc/SuperBuild/cmake/Modules/Buildlibsamplerate.cmake
@@ -10,5 +10,8 @@ ExternalProject_Add(
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/libsamplerate
     GIT_REPOSITORY https://github.com/libsndfile/libsamplerate.git
     GIT_TAG 0.2.1
+    PATCH_COMMAND ${CMAKE_COMMAND} -E copy
+        ${CMAKE_SOURCE_DIR}/libsamplerate-patch/CMakeLists.txt
+        ${CMAKE_CURRENT_BINARY_DIR}/libsamplerate/src/libsamplerate/CMakeLists.txt
     LIST_SEPARATOR |
     CMAKE_ARGS ${libsamplerate_ARGS})
