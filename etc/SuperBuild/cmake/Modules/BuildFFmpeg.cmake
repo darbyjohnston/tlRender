cab23caef5ff8c505beedcdcc5a19a9fdaa53364
Darby Johnston <darbyjohnston@yahoo.com>
2021-08-23 16:55:07 -0700

    Build fixes

--- etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
+++ etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
@@ -8,6 +8,19 @@ set(FFmpeg_DEBUG OFF)
 if(WIN32)
     # See the directions for building FFmpeg on Windows in "docs/build_windows.html".
 else()
+    set(FFmpeg_CFLAGS)
+    set(FFmpeg_CXXFLAGS)
+    set(FFmpeg_LDFLAGS)
+    if(APPLE)
+        set(FFmpeg_CFLAGS "${FFmpeg_CFLAGS} -mmacosx-version-min ${CMAKE_OSX_DEPLOYMENT_TARGET}")
+        set(FFmpeg_CXXFLAGS "${FFmpeg_CXXFLAGS} -mmacosx-version-min ${CMAKE_OSX_DEPLOYMENT_TARGET}")
+        set(FFmpeg_LDFLAGS "${FFmpeg_LDFLAGS} -mmacosx-version-min ${CMAKE_OSX_DEPLOYMENT_TARGET}")
+    endif()
+    if(FFmpeg_DEBUG)
+        set(FFmpeg_CFLAGS "${FFmpeg_CFLAGS} -g")
+        set(FFmpeg_CXXFLAGS "${FFmpeg_CXXFLAGS} -g")
+        set(FFmpeg_LDFLAGS "${FFmpeg_LDFLAGS} -g")
+    endif()
     set(FFmpeg_CONFIGURE_ARGS
         --prefix=${CMAKE_INSTALL_PREFIX}
         --disable-programs
@@ -20,6 +33,9 @@ else()
         --disable-audiotoolbox
         --disable-vaapi
         --enable-pic
+        --extra-cflags="${FFmpeg_CFLAGS}"
+        --extra-cxxflags="${FFmpeg_CXXFLAGS}"
+        --extra-ldflags="${FFmpeg_LDFLAGS}"
         --x86asmexe=${CMAKE_INSTALL_PREFIX}/bin/nasm)
     if(FFmpeg_SHARED_LIBS)
         set(FFmpeg_CONFIGURE_ARGS
@@ -30,9 +46,6 @@ else()
     if(FFmpeg_DEBUG)
         set(FFmpeg_CONFIGURE_ARGS
             ${FFmpeg_CONFIGURE_ARGS}
-            --extra-cflags=-g
-            --extra-cxxflags=-g
-            --extra-ldflags=-g
             --disable-optimizations
             --disable-stripping
             --enable-debug=3
