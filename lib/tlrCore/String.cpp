06cfdbe78eea90b737ae6d46c65c0c36a1985c8b
Darby Johnston <darbyjohnston@yahoo.com>
2021-09-06 16:30:47 -0700

    String fix

--- lib/tlrCore/String.cpp
+++ lib/tlrCore/String.cpp
@@ -205,7 +205,7 @@ namespace tlr
             int64_t tens = 1;
             for (int i = int(end) - 1; i >= 0; --i, tens *= 10)
             {
-                out += (static_cast<int64_t>(s[i]) - 48) * tens;
+                out += (static_cast<int64_t>(s[i] - 48)) * tens;
             }
 
             // Apply the sign.
@@ -223,7 +223,7 @@ namespace tlr
             size_t tens = 1;
             for (int i = int(size) - 1; i >= 0; --i, tens *= 10)
             {
-                out += (static_cast<size_t>(s[i]) - 48) * tens;
+                out += (static_cast<size_t>(s[i] - 48)) * tens;
             }
         }
 
