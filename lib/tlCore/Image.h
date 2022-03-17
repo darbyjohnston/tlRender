35122a71b10356ee8efecbcd634658b64eda69f3
Darby Johnston <darbyjohnston@yahoo.com>
2022-03-17 11:04:15 -0700

    Remove constexpr

--- lib/tlCore/Image.h
+++ lib/tlCore/Image.h
@@ -42,7 +42,7 @@ namespace tl
 
             constexpr bool operator == (const Size&) const noexcept;
             constexpr bool operator != (const Size&) const noexcept;
-            constexpr bool operator < (const Size&) const noexcept;
+            bool operator < (const Size&) const noexcept;
         };
 
         //! Get a bounding box with the given aspect ratio that fits within
