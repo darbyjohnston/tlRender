65ce166b257c880cce3a7b89b11fa84198a36106
Darby Johnston <darbyjohnston@yahoo.com>
2022-06-13 14:05:42 -0700

    Refactoring

--- lib/tlDevice/BMDOutputDevice.cpp
+++ lib/tlDevice/BMDOutputDevice.cpp
@@ -9,14 +9,10 @@
 #include <tlCore/Context.h>
 #include <tlCore/StringFormat.h>
 
-#include "platform.h"
-
 #include <algorithm>
 #include <array>
 #include <atomic>
 #include <iostream>
-#include <list>
-#include <mutex>
 #include <tuple>
 
 namespace tl
@@ -27,101 +23,79 @@ namespace tl
         {
             const size_t preroll = 3;
             const size_t pixelDataListMax = 3;
+        }
 
-            class DLVideoOutputCallback : public IDeckLinkVideoOutputCallback
+        DLVideoOutputCallback::DLVideoOutputCallback(const std::function<void(IDeckLinkVideoFrame*)>& callback) :
+            _refCount(1),
+            _callback(callback)
+        {}
+
+        HRESULT DLVideoOutputCallback::QueryInterface(REFIID iid, LPVOID* ppv)
+        {
+            *ppv = NULL;
+            return E_NOINTERFACE;
+        }
+
+        ULONG DLVideoOutputCallback::AddRef()
+        {
+            return ++_refCount;
+        }
+
+        ULONG DLVideoOutputCallback::Release()
+        {
+            --_refCount;
+            if (0 == _refCount)
             {
-            public:
-                DLVideoOutputCallback(const std::function<void(IDeckLinkVideoFrame*)>&);
-
-                HRESULT STDMETHODCALLTYPE ScheduledFrameCompleted(IDeckLinkVideoFrame*, BMDOutputFrameCompletionResult) override;
-                HRESULT STDMETHODCALLTYPE ScheduledPlaybackHasStopped() override;
-
-                HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, LPVOID* ppv) override;
-                ULONG STDMETHODCALLTYPE AddRef() override;
-                ULONG STDMETHODCALLTYPE Release() override;
-
-            private:
-                std::atomic<size_t> _refCount;
-                std::function<void(IDeckLinkVideoFrame*)> _callback;
-            };
-
-            DLVideoOutputCallback::DLVideoOutputCallback(const std::function<void(IDeckLinkVideoFrame*)>& callback) :
-                _refCount(1),
-                _callback(callback)
-            {}
-
-            HRESULT DLVideoOutputCallback::QueryInterface(REFIID iid, LPVOID* ppv)
-            {
-                *ppv = NULL;
-                return E_NOINTERFACE;
+                delete this;
+                return 0;
             }
+            return _refCount;
+        }
 
-            ULONG DLVideoOutputCallback::AddRef()
+        HRESULT	DLVideoOutputCallback::ScheduledFrameCompleted(
+            IDeckLinkVideoFrame* completedFrame,
+            BMDOutputFrameCompletionResult result)
+        {
+            if (_callback)
             {
-                return ++_refCount;
+                _callback(completedFrame);
             }
+            //std::cout << "result: " << getLabel(result) << std::endl;
+            return S_OK;
+        }
 
-            ULONG DLVideoOutputCallback::Release()
+        HRESULT	DLVideoOutputCallback::ScheduledPlaybackHasStopped()
+        {
+            return S_OK;
+        }
+
+        DLWrapper::~DLWrapper()
+        {
+            if (p)
             {
-                --_refCount;
-                if (0 == _refCount)
-                {
-                    delete this;
-                    return 0;
-                }
-                return _refCount;
+                p->Release();
             }
+        }
 
-            HRESULT	DLVideoOutputCallback::ScheduledFrameCompleted(
-                IDeckLinkVideoFrame* completedFrame,
-                BMDOutputFrameCompletionResult result)
+        DLOutputWrapper::~DLOutputWrapper()
+        {
+            if (p)
             {
-                if (_callback)
-                {
-                    _callback(completedFrame);
-                }
-                //std::cout << "result: " << getLabel(result) << std::endl;
-                return S_OK;
+                p->SetScheduledFrameCompletionCallback(nullptr);
+                p->Release();
             }
+        }
 
-            HRESULT	DLVideoOutputCallback::ScheduledPlaybackHasStopped()
+        DLVideoOutputCallbackWrapper::~DLVideoOutputCallbackWrapper()
+        {
+            if (p)
             {
-                return S_OK;
+                p->Release();
             }
         }
 
         namespace
         {
-            class DLWrapper
-            {
-            public:
-                IDeckLink* p = nullptr;
-                ~DLWrapper() { if (p) p->Release(); }
-            };
-
-            class DLOutputWrapper
-            {
-            public:
-                IDeckLinkOutput* p = nullptr;
-                ~DLOutputWrapper()
-                {
-                    if (p)
-                    {
-                        p->StopScheduledPlayback(0, nullptr, 0);
-                        p->DisableVideoOutput();
-                        p->SetScheduledFrameCompletionCallback(nullptr);
-                        p->Release();
-                    }
-                }
-            };
-
-            class DLVideoOutputCallbackWrapper
-            {
-            public:
-                DLVideoOutputCallback* p = nullptr;
-                ~DLVideoOutputCallbackWrapper() { if (p) p->Release(); }
-            };
-
             class DLIteratorWrapper
             {
             public:
@@ -151,21 +125,6 @@ namespace tl
             };
         }
 
-        struct BMDOutputDevice::Private
-        {
-            uint64_t frameCount = 0;
-            imaging::Size size;
-            otime::RationalTime frameRate;
-
-            std::list<std::shared_ptr<device::PixelData> > pixelData;
-            std::shared_ptr<device::PixelData> pixelDataTmp;
-            std::mutex pixelDataMutex;
-
-            DLWrapper dl;
-            DLOutputWrapper dlOutput;
-            DLVideoOutputCallbackWrapper dlVideoOutputCallback;
-        };
-
         void BMDOutputDevice::_init(
             int deviceIndex,
             int displayModeIndex,
@@ -173,8 +132,6 @@ namespace tl
             const std::shared_ptr<system::Context>& context)
         {
             IOutputDevice::_init(deviceIndex, displayModeIndex, pixelType, context);
-
-            TLRENDER_P();
 
             DLIteratorWrapper dlIterator;
             if (GetDeckLinkIterator(&dlIterator.p) != S_OK)
@@ -184,35 +141,35 @@ namespace tl
 
             int count = 0;
             std::string modelName;
-            while (dlIterator.p->Next(&p.dl.p) == S_OK)
+            while (dlIterator.p->Next(&_dl.p) == S_OK)
             {
                 if (count == deviceIndex)
                 {
                     dlstring_t dlModelName;
-                    p.dl.p ->GetModelName(&dlModelName);
+                    _dl.p->GetModelName(&dlModelName);
                     modelName = DlToStdString(dlModelName);
                     DeleteString(dlModelName);
 
                     break;
                 }
 
-                p.dl.p->Release();
-                p.dl.p = nullptr;
+                _dl.p->Release();
+                _dl.p = nullptr;
 
                 ++count;
             }
-            if (!p.dl.p)
+            if (!_dl.p)
             {
                 throw std::runtime_error("Device not found");
             }
 
-            if (p.dl.p->QueryInterface(IID_IDeckLinkOutput, (void**)&p.dlOutput) != S_OK)
+            if (_dl.p->QueryInterface(IID_IDeckLinkOutput, (void**)&_dlOutput) != S_OK)
             {
                 throw std::runtime_error("Output device not found");
             }
 
             DLDisplayModeIteratorWrapper dlDisplayModeIterator;
-            if (p.dlOutput.p->GetDisplayModeIterator(&dlDisplayModeIterator.p) != S_OK)
+            if (_dlOutput.p->GetDisplayModeIterator(&dlDisplayModeIterator.p) != S_OK)
             {
                 throw std::runtime_error("Cannot get display mode iterator");
             }
@@ -235,40 +192,40 @@ namespace tl
                 throw std::runtime_error("Display mode not found");
             }
 
-            p.size.w = dlDisplayMode.p->GetWidth();
-            p.size.h = dlDisplayMode.p->GetHeight();
+            _size.w = dlDisplayMode.p->GetWidth();
+            _size.h = dlDisplayMode.p->GetHeight();
             BMDTimeValue frameDuration;
             BMDTimeScale frameTimescale;
             dlDisplayMode.p->GetFrameRate(&frameDuration, &frameTimescale);
-            p.frameRate = otime::RationalTime(frameDuration, frameTimescale);
+            _frameRate = otime::RationalTime(frameDuration, frameTimescale);
 
             context->log(
                 "tl::device::BMDOutputDevice",
                 string::Format("#{0} {1} {2} {3}").
                 arg(deviceIndex).
                 arg(modelName).
-                arg(p.size).
-                arg(p.frameRate));
+                arg(_size).
+                arg(_frameRate));
 
-            if (p.dlOutput.p->EnableVideoOutput(
+            if (_dlOutput.p->EnableVideoOutput(
                 dlDisplayMode.p->GetDisplayMode(),
                 bmdVideoOutputFlagDefault) != S_OK)
             {
                 throw std::runtime_error("Cannot enable video output");
             }
 
-            p.dlVideoOutputCallback.p = new DLVideoOutputCallback(
+            _dlVideoOutputCallback.p = new DLVideoOutputCallback(
                 [this](IDeckLinkVideoFrame* dlVideoFrame)
                 {
                     std::shared_ptr<device::PixelData> pixelData;
                     {
-                        std::unique_lock<std::mutex> lock(_p->pixelDataMutex);
-                        if (!_p->pixelData.empty())
+                        std::unique_lock<std::mutex> lock(_pixelDataMutex);
+                        if (!_pixelData.empty())
                         {
-                            _p->pixelDataTmp = _p->pixelData.front();
-                            _p->pixelData.pop_front();
+                            _pixelDataTmp = _pixelData.front();
+                            _pixelData.pop_front();
                         }
-                        pixelData = _p->pixelDataTmp;
+                        pixelData = _pixelDataTmp;
                     }
                     if (pixelData)
                     {
@@ -277,17 +234,17 @@ namespace tl
                         dlVideoFrame->GetBytes((void**)&dlFrame);
                         memcpy(dlFrame, pixelData->getData(), pixelData->getDataByteCount());
                     }
-                    if (_p->dlOutput.p->ScheduleVideoFrame(
+                    if (_dlOutput.p->ScheduleVideoFrame(
                         dlVideoFrame,
-                        _p->frameCount * _p->frameRate.value(),
-                        _p->frameRate.value(),
-                        _p->frameRate.rate()) == S_OK)
+                        _frameCount * _frameRate.value(),
+                        _frameRate.value(),
+                        _frameRate.rate()) == S_OK)
                     {
-                        _p->frameCount = _p->frameCount + 1;
+                        _frameCount = _frameCount + 1;
                     }
                 });
 
-            if (p.dlOutput.p->SetScheduledFrameCompletionCallback(p.dlVideoOutputCallback.p) != S_OK)
+            if (_dlOutput.p->SetScheduledFrameCompletionCallback(_dlVideoOutputCallback.p) != S_OK)
             {
                 throw std::runtime_error("Cannot set callback");
             }
@@ -295,37 +252,36 @@ namespace tl
             DLVideoFrameWrapper dlVideoFrame;
             for (int i = 0; i < preroll; ++i)
             {
-                if (p.dlOutput.p->CreateVideoFrame(
-                    p.size.w,
-                    p.size.h,
-                    p.size.w * 4,
+                if (_dlOutput.p->CreateVideoFrame(
+                    _size.w,
+                    _size.h,
+                    _size.w * 4,
                     toBMD(pixelType),
                     bmdFrameFlagFlipVertical,
                     &dlVideoFrame.p) != S_OK)
                 {
                     throw std::runtime_error("Cannot create video frame");
                 }
-                if (p.dlOutput.p->ScheduleVideoFrame(
+                if (_dlOutput.p->ScheduleVideoFrame(
                     dlVideoFrame.p,
-                    p.frameCount * p.frameRate.value(),
-                    p.frameRate.value(),
-                    p.frameRate.rate()) != S_OK)
+                    _frameCount * _frameRate.value(),
+                    _frameRate.value(),
+                    _frameRate.rate()) != S_OK)
                 {
                     throw std::runtime_error("Cannot schedule video frame");
                 }
                 dlVideoFrame.p->Release();
                 dlVideoFrame.p = nullptr;
-                p.frameCount = p.frameCount + 1;
+                _frameCount = _frameCount + 1;
             }
 
-            p.dlOutput.p->StartScheduledPlayback(
+            _dlOutput.p->StartScheduledPlayback(
                 0,
-                p.frameRate.rate(),
+                _frameRate.rate(),
                 1.0);
         }
 
-        BMDOutputDevice::BMDOutputDevice() :
-            _p(new Private)
+        BMDOutputDevice::BMDOutputDevice()
         {}
 
         BMDOutputDevice::~BMDOutputDevice()
@@ -344,22 +300,21 @@ namespace tl
 
         const imaging::Size& BMDOutputDevice::getSize() const
         {
-            return _p->size;
+            return _size;
         }
 
         const otime::RationalTime& BMDOutputDevice::getFrameRate() const
         {
-            return _p->frameRate;
+            return _frameRate;
         }
 
         void BMDOutputDevice::display(const std::shared_ptr<device::PixelData>& pixelData)
         {
-            TLRENDER_P();
-            std::unique_lock<std::mutex> lock(p.pixelDataMutex);
-            p.pixelData.push_back(pixelData);
-            while (p.pixelData.size() > pixelDataListMax)
+            std::unique_lock<std::mutex> lock(_pixelDataMutex);
+            _pixelData.push_back(pixelData);
+            while (_pixelData.size() > pixelDataListMax)
             {
-                p.pixelData.pop_front();
+                _pixelData.pop_front();
             }
         }
     }
--- lib/tlDevice/BMDOutputDevice.h
+++ lib/tlDevice/BMDOutputDevice.h
@@ -6,10 +6,55 @@
 
 #include <tlDevice/IOutputDevice.h>
 
+#include "platform.h"
+
+#include <list>
+#include <mutex>
+
 namespace tl
 {
     namespace device
     {
+        class BMDOutputDevice;
+
+        class DLVideoOutputCallback : public IDeckLinkVideoOutputCallback
+        {
+        public:
+            DLVideoOutputCallback(const std::function<void(IDeckLinkVideoFrame*)>&);
+
+            HRESULT STDMETHODCALLTYPE ScheduledFrameCompleted(IDeckLinkVideoFrame*, BMDOutputFrameCompletionResult) override;
+            HRESULT STDMETHODCALLTYPE ScheduledPlaybackHasStopped() override;
+
+            HRESULT STDMETHODCALLTYPE QueryInterface(REFIID iid, LPVOID* ppv) override;
+            ULONG STDMETHODCALLTYPE AddRef() override;
+            ULONG STDMETHODCALLTYPE Release() override;
+
+        private:
+            std::atomic<size_t> _refCount;
+            std::function<void(IDeckLinkVideoFrame*)> _callback;
+        };
+
+        class DLVideoOutputCallbackWrapper
+        {
+        public:
+            DLVideoOutputCallback* p = nullptr;
+            ~DLVideoOutputCallbackWrapper();
+        };
+
+        class DLWrapper
+        {
+        public:
+            IDeckLink* p = nullptr;
+            ~DLWrapper();
+        };
+
+        class DLOutputWrapper
+        {
+        public:
+            IDeckLinkOutput* p = nullptr;
+            ~DLOutputWrapper();
+        };
+
         //! BMD output device.
         class BMDOutputDevice : public IOutputDevice
         {
@@ -39,7 +84,17 @@ namespace tl
             void display(const std::shared_ptr<device::PixelData>&) override;
 
         private:
-            TLRENDER_PRIVATE();
+            uint64_t _frameCount = 0;
+            imaging::Size _size;
+            otime::RationalTime _frameRate;
+
+            std::list<std::shared_ptr<device::PixelData> > _pixelData;
+            std::shared_ptr<device::PixelData> _pixelDataTmp;
+            std::mutex _pixelDataMutex;
+
+            DLWrapper _dl;
+            DLOutputWrapper _dlOutput;
+            DLVideoOutputCallbackWrapper _dlVideoOutputCallback;
         };
     }
 }
