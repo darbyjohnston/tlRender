f491b340520fbb1c3005988aacc39fef27acd3ce
Darby Johnston <darbyjohnston@yahoo.com>
2022-07-25 18:57:04 -0700

    Disable tests

--- etc/SuperBuild/cmake/Modules/Buildnlohmann_json.cmake
+++ etc/SuperBuild/cmake/Modules/Buildnlohmann_json.cmake
@@ -4,9 +4,7 @@ set(nlohmann_json_GIT_REPOSITORY "https://github.com/nlohmann/json.git")
 set(nlohmann_json_GIT_TAG "v3.10.5")
 
 set(nlohmann_json_ARGS ${TLRENDER_EXTERNAL_ARGS})
-if(BUILD_SHARED_LIBS)
-    list(APPEND nlohmann_json_ARGS -DJSON_BuildTests=OFF)
-endif()
+list(APPEND nlohmann_json_ARGS -DJSON_BuildTests=OFF)
 
 ExternalProject_Add(
 	nlohmann_json
