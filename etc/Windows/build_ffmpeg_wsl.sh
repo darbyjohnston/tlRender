9aaf65bd05fff72e44b38de618926ad957f8872d
Darby Johnston <darbyjohnston@yahoo.com>
2022-10-25 17:45:34 -0700

    Update FFmpeg version

--- etc/Windows/build_ffmpeg_wsl.sh
+++ etc/Windows/build_ffmpeg_wsl.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-wget https://ffmpeg.org/releases/ffmpeg-5.1.tar.bz2
-tar xvf ffmpeg-5.1.tar.bz2
-cd ffmpeg-5.1
+wget https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.bz2
+tar xvf ffmpeg-5.1.2.tar.bz2
+cd ffmpeg-5.1.2
 ./configure \
     --disable-programs \
     --disable-doc \
