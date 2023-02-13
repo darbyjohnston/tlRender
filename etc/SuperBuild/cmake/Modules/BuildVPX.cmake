89fe141195af426f96f25ccf0b64f5cb2386a2ad
Gonzalo Garramuño <ggarra13@gmail.com>
2023-02-13 18:05:07 -0300

    Requested changes (typos, indentation and use of CMAKE_INSTALL_PREFIX Instead of CMAKE_PREFIX_PATH).

--- etc/SuperBuild/cmake/Modules/BuildVPX.cmake
+++ etc/SuperBuild/cmake/Modules/BuildVPX.cmake
@@ -3,7 +3,7 @@ include(ExternalProject)
 
 # set(LIBVPX_TAG main) # live on the cutting-edge!
 
-set(VPX_TAG v1.12.0) # ptoven to work
+set(VPX_TAG v1.12.0) # proven to work
 
 
 if(WIN32 OR NOT TLRENDER_FFMPEG)
@@ -16,22 +16,22 @@ else()
     set(VPX_LDFLAGS)
 
     set(VPX_CONFIGURE_ARGS
-        --prefix=${CMAKE_INSTALL_PREFIX}
-        --enable-pic
-        --disable-examples
-        --disable-tools
-        --disable-docs
-        --disable-unit-tests
-        )
+	--prefix=${CMAKE_INSTALL_PREFIX}
+	--enable-pic
+	--disable-examples
+	--disable-tools
+	--disable-docs
+	--disable-unit-tests
+	)
 
     ExternalProject_Add(
-        VPX
-        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/VPX
-        DEPENDS YASM
-        GIT_REPOSITORY "https://github.com/webmproject/libvpx.git"
-        GIT_TAG ${VPX_TAG}
-        CONFIGURE_COMMAND PATH=${CMAKE_PREFIX_PATH}/bin:$ENV{PATH} ./configure ${VPX_CONFIGURE_ARGS}
-        BUILD_IN_SOURCE 1
-        )
+	VPX
+	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/VPX
+	DEPENDS YASM
+	GIT_REPOSITORY "https://github.com/webmproject/libvpx.git"
+	GIT_TAG ${VPX_TAG}
+	CONFIGURE_COMMAND PATH=${CMAKE_PREFIX_PATH}/bin:$ENV{PATH} ./configure ${VPX_CONFIGURE_ARGS}
+	BUILD_IN_SOURCE 1
+	)
 
 endif()
