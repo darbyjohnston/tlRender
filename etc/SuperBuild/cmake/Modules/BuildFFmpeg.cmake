ecdd3b18c182cdd1acb9bc8393cfaca6485eba89
Gonzalo Garramuño <ggarra13@gmail.com>
2023-02-13 18:59:57 -0300

    Untabify emacs change (sorry Darby, I did not update Windows emacs).

--- etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
+++ etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
@@ -14,74 +14,74 @@ else()
     set(FFmpeg_LDFLAGS)
 
     if(APPLE AND CMAKE_OSX_DEPLOYMENT_TARGET)
-	list(APPEND FFmpeg_CFLAGS "--extra-cflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
-	list(APPEND FFmpeg_CXXFLAGS "--extra-cxxflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
-	list(APPEND FFmpeg_OBJCFLAGS "--extra-objcflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
-	list(APPEND FFmpeg_LDFLAGS "--extra-ldflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+        list(APPEND FFmpeg_CFLAGS "--extra-cflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+        list(APPEND FFmpeg_CXXFLAGS "--extra-cxxflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+        list(APPEND FFmpeg_OBJCFLAGS "--extra-objcflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+        list(APPEND FFmpeg_LDFLAGS "--extra-ldflags=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
     endif()
     if(TLRENDER_VPX)
-	list(APPEND FFmpeg_CONFIGURE_ARGS
-	    --enable-libvpx)
-	#
-	# Make sure we pick the static libvpx we compiled, not the system one
-	#
-	list(APPEND FFmpeg_LDFLAGS
-	    --extra-ldflags="${CMAKE_INSTALL_PREFIX}/lib/libvpx.a")
-	list(APPEND FFmpeg_DEPS VPX)
+        list(APPEND FFmpeg_CONFIGURE_ARGS
+            --enable-libvpx)
+        #
+        # Make sure we pick the static libvpx we compiled, not the system one
+        #
+        list(APPEND FFmpeg_LDFLAGS
+            --extra-ldflags="${CMAKE_INSTALL_PREFIX}/lib/libvpx.a")
+        list(APPEND FFmpeg_DEPS VPX)
     endif()
     if(FFmpeg_DEBUG)
-	list(APPEND FFmpeg_CFLAGS "--extra-cflags=-g")
-	list(APPEND FFmpeg_CXXFLAGS "--extra-cxxflags=-g")
-	list(APPEND FFmpeg_OBJCFLAGS "--extra-objcflags=-g")
-	list(APPEND FFmpeg_LDFLAGS "--extra-ldflags=-g")
+        list(APPEND FFmpeg_CFLAGS "--extra-cflags=-g")
+        list(APPEND FFmpeg_CXXFLAGS "--extra-cxxflags=-g")
+        list(APPEND FFmpeg_OBJCFLAGS "--extra-objcflags=-g")
+        list(APPEND FFmpeg_LDFLAGS "--extra-ldflags=-g")
     endif()
     set(FFmpeg_CONFIGURE_ARGS
-	--prefix=${CMAKE_INSTALL_PREFIX}
-	--disable-programs
-	--disable-bzlib
-	--disable-iconv
-	--disable-lzma
-	--disable-appkit
-	--disable-avfoundation
-	--disable-coreimage
-	--disable-audiotoolbox
-	--disable-vaapi
-	--disable-sdl2
-	--enable-pic
-	${FFmpeg_CFLAGS}
-	${FFmpeg_CXXFLAGS}
-	${FFmpeg_OBJCFLAGS}
-	${FFmpeg_LDFLAGS}
-	--x86asmexe=${CMAKE_INSTALL_PREFIX}/bin/nasm)
+        --prefix=${CMAKE_INSTALL_PREFIX}
+        --disable-programs
+        --disable-bzlib
+        --disable-iconv
+        --disable-lzma
+        --disable-appkit
+        --disable-avfoundation
+        --disable-coreimage
+        --disable-audiotoolbox
+        --disable-vaapi
+        --disable-sdl2
+        --enable-pic
+        ${FFmpeg_CFLAGS}
+        ${FFmpeg_CXXFLAGS}
+        ${FFmpeg_OBJCFLAGS}
+        ${FFmpeg_LDFLAGS}
+        --x86asmexe=${CMAKE_INSTALL_PREFIX}/bin/nasm)
     if (APPLE AND CMAKE_OSX_ARCHITECTURES)
-	list(APPEND FFmpeg_CONFIGURE_ARGS
-	    --arch=${CMAKE_OSX_ARCHITECTURES})
+        list(APPEND FFmpeg_CONFIGURE_ARGS
+            --arch=${CMAKE_OSX_ARCHITECTURES})
     endif()
     if(UNIX)
-	list(APPEND FFmpeg_CONFIGURE_ARGS
-	    --disable-libxcb
-	    --disable-libxcb-shm
-	    --disable-libxcb-xfixes
-	    --disable-libxcb-shape
-	    --disable-xlib)
+        list(APPEND FFmpeg_CONFIGURE_ARGS
+            --disable-libxcb
+            --disable-libxcb-shm
+            --disable-libxcb-xfixes
+            --disable-libxcb-shape
+            --disable-xlib)
     endif()
     if(FFmpeg_SHARED_LIBS)
-	list(APPEND FFmpeg_CONFIGURE_ARGS
-	    --disable-static
-	    --enable-shared)
+        list(APPEND FFmpeg_CONFIGURE_ARGS
+            --disable-static
+            --enable-shared)
     endif()
     if(FFmpeg_DEBUG)
-	list(APPEND FFmpeg_CONFIGURE_ARGS
-	    --disable-optimizations
-	    --disable-stripping
-	    --enable-debug=3
-	    --assert-level=2)
+        list(APPEND FFmpeg_CONFIGURE_ARGS
+            --disable-optimizations
+            --disable-stripping
+            --enable-debug=3
+            --assert-level=2)
     endif()
     ExternalProject_Add(
-	FFmpeg
-	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/FFmpeg
-	DEPENDS ${FFmpeg_DEPS}
-	URL https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.bz2
-	CONFIGURE_COMMAND ./configure ${FFmpeg_CONFIGURE_ARGS}
-	BUILD_IN_SOURCE 1)
+        FFmpeg
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/FFmpeg
+        DEPENDS ${FFmpeg_DEPS}
+        URL https://ffmpeg.org/releases/ffmpeg-5.1.2.tar.bz2
+        CONFIGURE_COMMAND ./configure ${FFmpeg_CONFIGURE_ARGS}
+        BUILD_IN_SOURCE 1)
 endif()
--- etc/SuperBuild/cmake/Modules/BuildVPX.cmake
+++ etc/SuperBuild/cmake/Modules/BuildVPX.cmake
@@ -16,22 +16,22 @@ else()
     set(VPX_LDFLAGS)
 
     set(VPX_CONFIGURE_ARGS
-	--prefix=${CMAKE_INSTALL_PREFIX}
-	--enable-pic
-	--disable-examples
-	--disable-tools
-	--disable-docs
-	--disable-unit-tests
-	)
+        --prefix=${CMAKE_INSTALL_PREFIX}
+        --enable-pic
+        --disable-examples
+        --disable-tools
+        --disable-docs
+        --disable-unit-tests
+        )
 
     ExternalProject_Add(
-	VPX
-	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/VPX
-	DEPENDS YASM
-	GIT_REPOSITORY "https://github.com/webmproject/libvpx.git"
-	GIT_TAG ${VPX_TAG}
-	CONFIGURE_COMMAND PATH=${CMAKE_PREFIX_PATH}/bin:$ENV{PATH} ./configure ${VPX_CONFIGURE_ARGS}
-	BUILD_IN_SOURCE 1
-	)
+        VPX
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/VPX
+        DEPENDS YASM
+        GIT_REPOSITORY "https://github.com/webmproject/libvpx.git"
+        GIT_TAG ${VPX_TAG}
+        CONFIGURE_COMMAND PATH=${CMAKE_PREFIX_PATH}/bin:$ENV{PATH} ./configure ${VPX_CONFIGURE_ARGS}
+        BUILD_IN_SOURCE 1
+        )
 
 endif()
