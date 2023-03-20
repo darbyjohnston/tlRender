28ec63596f6a9e01181ca43cf9016689abff2f90
Gonzalo Garramuño <ggarra13@gmail.com>
2023-03-20 19:46:32 -0300

    Fixed BUILD_COMMAND to also change PATH variable.

--- etc/SuperBuild/cmake/Modules/BuildVPX.cmake
+++ etc/SuperBuild/cmake/Modules/BuildVPX.cmake
@@ -16,22 +16,25 @@ else()
     set(VPX_LDFLAGS)
 
     set(VPX_CONFIGURE_ARGS
-        --prefix=${CMAKE_INSTALL_PREFIX}
-        --enable-pic
-        --disable-examples
-        --disable-tools
-        --disable-docs
-        --disable-unit-tests
-    )
+	--prefix=${CMAKE_INSTALL_PREFIX}
+	--enable-pic
+	--disable-examples
+	--disable-tools
+	--disable-docs
+	--disable-unit-tests
+	)
 
+    set( YASM_BIN_PATH "${CMAKE_INSTALL_PREFIX}/bin:$ENV{PATH}" )
+    
     ExternalProject_Add(
 	VPX
 	PREFIX ${CMAKE_CURRENT_BINARY_DIR}/VPX
-	DEPENDS NASM ${TLRENDER_YASM_DEP}
+	DEPENDS YASM
 	GIT_REPOSITORY "https://github.com/webmproject/libvpx.git"
 	GIT_TAG ${VPX_TAG}
-	CONFIGURE_COMMAND PATH=${CMAKE_INSTALL_PREFIX}/bin:$ENV{PATH} ./configure ${VPX_CONFIGURE_ARGS}
+	CONFIGURE_COMMAND PATH=${YASM_BIN_PATH} ./configure ${VPX_CONFIGURE_ARGS}
+	BUILD_COMMAND PATH=${YASM_BIN_PATH} make
 	BUILD_IN_SOURCE 1
-    )
+	)
 
 endif()
