3efea69d6554bef5d9712cbea9b3fdfdd01a5600
Darby Johnston <darbyjohnston@yahoo.com>
2023-04-07 09:41:09 -0700

    Update FreeType, NASM, and nlohmann_json versions

--- etc/SuperBuild/cmake/Modules/BuildNASM.cmake
+++ etc/SuperBuild/cmake/Modules/BuildNASM.cmake
@@ -2,7 +2,7 @@ include(ExternalProject)
 
 ExternalProject_Add(
     NASM
-    URL http://www.nasm.us/pub/nasm/releasebuilds/2.15.05/nasm-2.15.05.tar.xz
+    URL http://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.xz
     CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX}
     BUILD_IN_SOURCE 1)
 
