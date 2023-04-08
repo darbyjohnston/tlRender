9485d990449397cccd5e7c3c2988b4dcd416997f
Darby Johnston <darbyjohnston@yahoo.com>
2023-04-07 19:08:52 -0700

    Update OpenColorIO version

---
+++ etc/SuperBuild/cmake/Modules/Buildyaml-cpp.cmake
@@ -0,0 +1,18 @@
+include(ExternalProject)
+
+set(yaml-cpp_GIT_REPOSITORY "https://github.com/jbeder/yaml-cpp.git")
+set(yaml-cpp_GIT_TAG "yaml-cpp-0.7.0")
+
+set(yaml-cpp_ARGS
+    -DYAML_CPP_BUILD_CONTRIB=OFF
+    -DYAML_CPP_BUILD_TOOLS=OFF
+    -DYAML_CPP_BUILD_TESTS=OFF
+    ${TLRENDER_EXTERNAL_ARGS})
+
+ExternalProject_Add(
+    yaml-cpp
+    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/yaml-cpp
+    GIT_REPOSITORY ${yaml-cpp_GIT_REPOSITORY}
+    GIT_TAG ${yaml-cpp_GIT_TAG}
+    LIST_SEPARATOR |
+    CMAKE_ARGS ${yaml-cpp_ARGS})
