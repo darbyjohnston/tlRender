41a72914470ee9a9d958bc3652e0abd17a610256
Darby Johnston <darbyjohnston@yahoo.com>
2023-04-08 08:07:24 -0700

    Disable options

--- etc/SuperBuild/cmake/Modules/Buildminizip-ng.cmake
+++ etc/SuperBuild/cmake/Modules/Buildminizip-ng.cmake
@@ -4,9 +4,17 @@ set(minizip-ng_GIT_REPOSITORY "https://github.com/zlib-ng/minizip-ng.git")
 set(minizip-ng_GIT_TAG "3.0.9")
 
 set(minizip-ng_ARGS
-    -DMZ_FETCH_LIBS=OFF
+    -DMZ_BZIP2=OFF
     -DMZ_LZMA=OFF
     -DMZ_ZSTD=OFF
+    -DMZ_LIBCOMP=OFF
+    -DMZ_FETCH_LIBS=OFF
+    -DMZ_PKCRYPT=OFF
+    -DMZ_WZAES=OFF
+    -DMZ_OPENSSL=OFF
+    -DMZ_BCRYPT=OFF
+    -DMZ_LIBBSD=OFF
+    -DMZ_ICONV=OFF
     ${TLRENDER_EXTERNAL_ARGS})
 
 ExternalProject_Add(
