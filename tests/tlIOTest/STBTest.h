67f5a015956555d7a25834e03b7588df0186dea0
Gonzalo Garramuño <ggarra13@gmail.com>
2023-06-21 05:16:39 -0300

    Added STB test.

---
+++ tests/tlIOTest/STBTest.h
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2021-2023 Darby Johnston
+// All rights reserved.
+
+#pragma once
+
+#include <tlTestLib/ITest.h>
+
+namespace tl
+{
+    namespace io_tests
+    {
+        class STBTest : public tests::ITest
+        {
+        protected:
+            STBTest(const std::shared_ptr<system::Context>&);
+
+        public:
+            static std::shared_ptr<STBTest> create(const std::shared_ptr<system::Context>&);
+
+            void run() override;
+
+        private:
+            void _io();
+        };
+    }
+}
