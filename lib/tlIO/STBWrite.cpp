974b87baca75bf2e33cf40aa31955557555a33d0
Gonzalo Garramuño <ggarra13@gmail.com>
2023-06-28 17:24:31 -0300

    Added TGA, BMP and PSD readers.  Added TGA and BMP writers. (#100)
    * Added TGA, BMP and PSD readers.  Added TGA and BMP writers.
    
    They are very light as they rely on two auxiliary .h files
    (stb_image.h and stb_image_writer.h).
    
    * Made pixeType support alpha if possible.
    
    * Added STB to IOSystem and CMakeLists optional build.
    
    * Moved stb .h files to deps/stb.
    
    Added reading from memory.
    
    * Fixed typo.
    
    Updated to darby/main.
    
    * Fixed darby/main sync.
    
    * Added STB test.
    
    * Fixed Unit Test.  BMP is fine.  TGA reads are fine.  TGA write of 1x1 fails.
    
    * Added UTF-8 define to STBRead.cpp.
    
    * Fixed opening brace.

---
+++ lib/tlIO/STBWrite.cpp
@@ -0,0 +1,95 @@
+// SPDX-License-Identifier: BSD-3-Clause
+// Copyright (c) 2021-2023 Darby Johnston
+// All rights reserved.
+
+#define STBIW_WINDOWS_UTF8
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include <stb/stb_image_write.h>
+
+#include <tlIO/STB.h>
+
+#include <tlCore/StringFormat.h>
+
+namespace tl
+{
+    namespace stb
+    {
+        namespace
+        {
+            class File
+            {
+            public:
+                File(
+                    const std::string& fileName,
+                    const std::shared_ptr<imaging::Image>& image)
+                {
+                    const auto& info = image->getInfo();
+                    const int comp = static_cast<int>(
+                        imaging::getChannelCount(info.pixelType));
+                    const size_t bytes = imaging::getBitDepth(info.pixelType) / 8;
+                    if (bytes > 1)
+                        throw std::runtime_error(string::Format("{0}: {1}").
+                            arg(fileName).
+                            arg("Unsupported image depth"));
+                    
+                    stbi_flip_vertically_on_write(1);
+    
+                    file::Path path(fileName);
+                    std::string ext = path.getExtension();
+                    int res = 0;
+                    if (string::compareNoCase(ext, ".tga"))
+                        res = stbi_write_tga(
+                            fileName.c_str(), info.size.w, info.size.h, comp,
+                            image->getData());
+                    else if (string::compareNoCase(ext, ".bmp"))
+                        res = stbi_write_bmp(
+                            fileName.c_str(), info.size.w, info.size.h, comp,
+                            image->getData());
+                    else
+                        throw std::runtime_error(string::Format("{0}: {1}").
+                            arg(fileName).
+                            arg("Unsupported image format"));
+                    
+                    if (res == 0)
+                        throw std::runtime_error(string::Format("{0}: {1}").
+                            arg(fileName).
+                            arg("Save image failed"));
+                }
+            };
+        }
+
+        void Write::_init(
+            const file::Path& path,
+            const io::Info& info,
+            const io::Options& options,
+            const std::weak_ptr<log::System>& logSystem)
+        {
+            ISequenceWrite::_init(path, info, options, logSystem);
+        }
+
+        Write::Write()
+        {}
+
+        Write::~Write()
+        {}
+
+        std::shared_ptr<Write> Write::create(
+            const file::Path& path,
+            const io::Info& info,
+            const io::Options& options,
+            const std::weak_ptr<log::System>& logSystem)
+        {
+            auto out = std::shared_ptr<Write>(new Write);
+            out->_init(path, info, options, logSystem);
+            return out;
+        }
+
+        void Write::_writeVideo(
+            const std::string& fileName,
+            const otime::RationalTime&,
+            const std::shared_ptr<imaging::Image>& image)
+        {
+            const auto f = File(fileName, image);
+        }
+    }
+}
