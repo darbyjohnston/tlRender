a10a8f89a9b5ec20e8eda23006bc7949af9b0c40
Darby Johnston <darbyjohnston@yahoo.com>
2023-07-12 14:36:32 -0700

    macOS x86_64 fixes

--- etc/SuperBuild/cmake/Modules/BuildUSD.cmake
+++ etc/SuperBuild/cmake/Modules/BuildUSD.cmake
@@ -2,13 +2,26 @@ include(ExternalProject)
 
 set(USD_DEPS)
 
+set(USD_ARGS)
+if(CMAKE_OSX_ARCHITECTURES)
+    list(APPEND USD_ARGS --build-target ${CMAKE_OSX_ARCHITECTURES})
+endif()
+if(CMAKE_OSX_DEPLOYMENT_TARGET)
+    list(APPEND USD_ARGS --build-args)
+    list(APPEND USD_ARGS USD,"-DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+    list(APPEND USD_ARGS OpenSubdiv,"-DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+    list(APPEND USD_ARGS MaterialX,"-DCMAKE_OSX_DEPLOYMENT_TARGET=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+    #list(APPEND USD_ARGS TBB,"CFLAGS=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET} CXXFLAGS=-mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET}")
+endif()
+list(APPEND USD_ARGS --no-python)
+
 ExternalProject_Add(
     USD
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/USD
     DEPENDS ${USD_DEPS}
     URL https://github.com/PixarAnimationStudios/USD/archive/refs/tags/v23.05.tar.gz
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND python3 build_scripts/build_usd.py --no-python ${CMAKE_INSTALL_PREFIX}
+    BUILD_COMMAND python3 build_scripts/build_usd.py ${USD_ARGS} ${CMAKE_INSTALL_PREFIX}
     BUILD_IN_SOURCE 1
     INSTALL_COMMAND "")
 
