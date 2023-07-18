a4cf69e498a4861c7f5ce8538dfc1b391e8350fb
Darby Johnston <darbyjohnston@yahoo.com>
2023-07-17 18:14:00 -0700

    NASM fix

--- etc/SuperBuild/cmake/Modules/BuildNASM.cmake
+++ etc/SuperBuild/cmake/Modules/BuildNASM.cmake
@@ -2,7 +2,9 @@ include(ExternalProject)
 
 ExternalProject_Add(
     NASM
-    URL https://github.com/netwide-assembler/nasm/archive/refs/tags/nasm-2.16.01.tar.gz
+    https://github.com/netwide-assembler/nasm/archive/refs/tags/nasm-2.15.05.tar.gz
+    PATCH_COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        ${CMAKE_CURRENT_SOURCE_DIR}/NASM-patch/configure
+        ${CMAKE_CURRENT_BINARY_DIR}/NASM/src/NASM/configure
     CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX}
     BUILD_IN_SOURCE 1)
-
