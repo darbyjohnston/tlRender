e5a2554341328a6c816ab6890cc74eee361b9e23
Darby Johnston <darbyjohnston@yahoo.com>
2023-07-17 17:43:49 -0700

    Use GitHub URL

--- etc/SuperBuild/cmake/Modules/BuildNASM.cmake
+++ etc/SuperBuild/cmake/Modules/BuildNASM.cmake
@@ -2,7 +2,7 @@ include(ExternalProject)
 
 ExternalProject_Add(
     NASM
-    URL http://www.nasm.us/pub/nasm/releasebuilds/2.16.01/nasm-2.16.01.tar.xz
+    URL https://github.com/netwide-assembler/nasm/archive/refs/tags/nasm-2.16.01.tar.gz
     CONFIGURE_COMMAND ./configure --prefix=${CMAKE_INSTALL_PREFIX}
     BUILD_IN_SOURCE 1)
 
