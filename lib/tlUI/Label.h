fb60ae540f07fedee7f11b055b29aa57967a6676
Darby Johnston <darbyjohnston@yahoo.com>
2023-07-31 12:49:07 -0700

    Add todo

--- lib/tlUI/Label.h
+++ lib/tlUI/Label.h
@@ -11,6 +11,8 @@ namespace tl
     namespace ui
     {
         //! Text label.
+        //! 
+        //! \todo Add text wrapping.
         class Label : public IWidget
         {
             TLRENDER_NON_COPYABLE(Label);
