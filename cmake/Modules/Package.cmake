f181b59b3f3b5b4d1ccf0c8c02b1a35bb27af2a8
Darby Johnston <darbyjohnston@yahoo.com>
2023-08-26 16:55:53 -0700

    Packaging

--- cmake/Modules/Package.cmake
+++ cmake/Modules/Package.cmake
@@ -93,9 +93,172 @@ if(WIN32)
         install(
             DIRECTORY ${CMAKE_INSTALL_PREFIX}/bin/usd
             DESTINATION bin)
+        install(
+            DIRECTORY ${CMAKE_INSTALL_PREFIX}/plugin
+            DESTINATION ".")
     endif()
     
     install(
         FILES ${INSTALL_DLLS}
         DESTINATION bin)
+
+elseif(APPLE)
+
+    set(CPACK_GENERATOR TGZ)
+
+    list(APPEND CMAKE_INSTALL_RPATH
+        "@executable_path/../lib")
+    #set(CMAKE_INSTALL_RPATH_USE_LINK_PATH TRUE)
+
+    set(INSTALL_DYLIBS)
+    
+    if(TLRENDER_FFMPEG)
+        set(FFMPEG_DYLIBS
+            ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.60.3.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.60.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.60.1.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.60.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavfilter.9.3.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavfilter.9.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavfilter.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavformat.60.3.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavformat.60.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavformat.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavutil.58.2.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavutil.58.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libavutil.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswresample.4.10.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswresample.4.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswresample.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswscale.7.1.100.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswscale.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libswscale.dylib)
+        list(APPEND INSTALL_DYLIBS ${FFMPEG_DYLIBS})
+    endif()
+    
+    if(TLRENDER_USD)
+        set(BOOST_DYLIBS
+            ${CMAKE_INSTALL_PREFIX}/lib/libboost_atomic.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libboost_regex.dylib)
+        set(MATERIALX_DYLIBS
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXCore.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXCore.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXCore.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXFormat.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXFormat.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXFormat.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenGlsl.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenGlsl.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenGlsl.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenMdl.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenMdl.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenMdl.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenMsl.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenMsl.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenMsl.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenOsl.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenOsl.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenOsl.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenShader.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenShader.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXGenShader.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRender.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRender.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRender.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderGlsl.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderGlsl.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderGlsl.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderHw.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderHw.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderHw.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderMsl.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderMsl.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderMsl.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderOsl.1.38.7.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderOsl.1.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXRenderOsl.dylib)
+        set(TBB_DYLIBS
+            ${CMAKE_INSTALL_PREFIX}/lib/libtbb.dylib)
+            #${CMAKE_INSTALL_PREFIX}/lib/libtbb_debug.dylib
+            #${CMAKE_INSTALL_PREFIX}/lib/libtbbmalloc.dylib
+            #${CMAKE_INSTALL_PREFIX}/lib/libtbbmalloc_debug.dylib
+            #${CMAKE_INSTALL_PREFIX}/lib/libtbbmalloc_proxy.dylib
+            #${CMAKE_INSTALL_PREFIX}/lib/libtbbmalloc_proxy_debug.dylib)
+        set(OSD_DYLIBS
+            ${CMAKE_INSTALL_PREFIX}/lib/libosdCPU.3.5.0.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libosdCPU.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libosdGPU.3.5.0.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libosdGPU.dylib)
+        set(USD_DYLIBS
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_ar.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_arch.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_cameraUtil.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_garch.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_geomUtil.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_gf.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_glf.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hd.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hdGp.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hdMtlx.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hdSt.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hdar.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hdsi.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hdx.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hf.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hgi.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hgiGL.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hgiInterop.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hgiMetal.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_hio.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_js.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_kind.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_ndr.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_pcp.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_plug.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_pxOsd.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_sdf.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_sdr.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_tf.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_trace.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usd.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdAppUtils.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdBakeMtlx.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdGeom.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdHydra.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdImaging.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdImagingGL.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdLux.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdMedia.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdMtlx.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdPhysics.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdProc.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdProcImaging.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdRender.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdRi.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdRiImaging.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdShade.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdSkel.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdSkelImaging.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdUI.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdUtils.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdVol.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_usdVolImaging.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_vt.dylib
+            ${CMAKE_INSTALL_PREFIX}/lib/libusd_work.dylib)
+        list(APPEND INSTALL_DYLIBS ${BOOST_DYLIBS} ${MATERIALX_DYLIBS} ${TBB_DYLIBS} ${OSD_DYLIBS} ${USD_DYLIBS})
+
+        install(
+            DIRECTORY ${CMAKE_INSTALL_PREFIX}/lib/usd
+            DESTINATION lib)
+        install(
+            DIRECTORY ${CMAKE_INSTALL_PREFIX}/plugin
+            DESTINATION ".")
+    endif()
+    
+    install(
+        FILES ${INSTALL_DYLIBS}
+        DESTINATION lib)
+
 endif()
--- etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
+++ etc/SuperBuild/cmake/Modules/BuildFFmpeg.cmake
@@ -42,6 +42,10 @@ else()
         ${FFmpeg_OBJCFLAGS}
         ${FFmpeg_LDFLAGS}
         --x86asmexe=${CMAKE_INSTALL_PREFIX}/bin/nasm)
+    #if(APPLE)
+    #    list(APPEND FFmpeg_CONFIGURE_ARGS
+    #        --enable-rpath)
+    #endif()
     if(UNIX)
         list(APPEND FFmpeg_CONFIGURE_ARGS
             --disable-libxcb
@@ -62,12 +66,23 @@ else()
             --enable-debug=3
             --assert-level=2)
     endif()
+    set(FFmpeg_INSTALL make install)
+    if(APPLE)
+        list(APPEND FFmpeg_INSTALL
+            COMMAND install_name_tool -id @rpath/libavcodec.60.3.100.dylib ${CMAKE_INSTALL_PREFIX}/lib/libavcodec.60.dylib
+            COMMAND install_name_tool -id @rpath/libavdevice.60.1.100.dylib ${CMAKE_INSTALL_PREFIX}/lib/libavdevice.60.dylib
+            COMMAND install_name_tool -id @rpath/libavfilter.9.3.100.dylib ${CMAKE_INSTALL_PREFIX}/lib/libavfilter.9.dylib
+            COMMAND install_name_tool -id @rpath/libavformat.60.3.100.dylib ${CMAKE_INSTALL_PREFIX}/lib/libavformat.60.dylib
+            COMMAND install_name_tool -id @rpath/libavutil.58.2.100.dylib ${CMAKE_INSTALL_PREFIX}/lib/libavutil.58.dylib
+            COMMAND install_name_tool -id @rpath/libswresample.4.10.100.dylib ${CMAKE_INSTALL_PREFIX}/lib/libswresample.4.dylib
+            COMMAND install_name_tool -id @rpath/libswscale.7.1.100.dylib ${CMAKE_INSTALL_PREFIX}/lib/libswscale.7.dylib)
+    endif()
     ExternalProject_Add(
         FFmpeg
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/FFmpeg
         DEPENDS ${FFmpeg_DEPS}
         URL https://ffmpeg.org/releases/ffmpeg-6.0.tar.bz2
         CONFIGURE_COMMAND ./configure ${FFmpeg_CONFIGURE_ARGS}
+        INSTALL_COMMAND ${FFmpeg_INSTALL}
         BUILD_IN_SOURCE 1)
 endif()
-
