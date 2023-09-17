0af2fbc98348a36e00d01be0536eeb46b2b3cc77
Darby Johnston <darbyjohnston@yahoo.com>
2023-09-17 12:52:50 -0700

    Disable OpenEXR threading to work around a crash at shutdown

--- etc/SuperBuild/cmake/Modules/BuildOpenEXR.cmake
+++ etc/SuperBuild/cmake/Modules/BuildOpenEXR.cmake
@@ -3,11 +3,15 @@ include(ExternalProject)
 set(OpenEXR_GIT_REPOSITORY "https://github.com/AcademySoftwareFoundation/openexr.git")
 set(OpenEXR_GIT_TAG "v3.2.0")
 
+# \bug Disable OpenEXR threading to work around a crash at shutdown in the
+# OpenEXR thread pool. Note that we already set the OpenEXR global thread
+# count to zero since we load frames in parallel.
 set(OpenEXR_ARGS
     ${TLRENDER_EXTERNAL_ARGS}
     -DOPENEXR_BUILD_TOOLS=OFF
     -DOPENEXR_INSTALL_EXAMPLES=OFF
-    -DBUILD_TESTING=OFF)
+    -DBUILD_TESTING=OFF
+    -DOPENEXR_ENABLE_THREADING=OFF)
 
 ExternalProject_Add(
     OpenEXR
