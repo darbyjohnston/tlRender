66494f92555f5cc825507c00dec4b8fb889510be
Darby Johnston <darbyjohnston@yahoo.com>
2023-09-27 17:56:55 -0700

    Move workflow into a script

---
+++ etc/Linux/gha-ubuntu-latest.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+set -x
+
+# Update packages
+sudo apt-get update
+
+# Install lcov
+if [[ $TLRENDER_GCOV = "ON" ]]
+then
+    sudo apt-get install lcov
+fi
+
+# Install OpenGL dev
+sudo apt-get install xorg-dev libglu1-mesa-dev mesa-common-dev
+
+# Install ALSA dev
+sudo apt-get install libasound2-dev
+
+# Install PulseAudio dev
+sudo apt-get install libpulse-dev
+
+# Install Python dev
+if [[ $TLRENDER_PYTHON = "ON" ]]
+then
+    sudo apt-get install python3.8-dev
+fi
+
+# Install Qt dev
+if [[ $TLRENDER_QT5 = "ON" ]]
+then
+    sudo apt-get install qtdeclarative5-dev libqt5quick5 qtbase5-dev libqt5svg5-dev qtchooser qt5-qmake qtbase5-dev-tools
+fi
+
+# Build tlRender
+mkdir build
+cd build
+cmake ../etc/SuperBuild \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_INSTALL_PREFIX=$PWD/install \
+    -DCMAKE_PREFIX_PATH=$PWD/install \
+    -DTLRENDER_MMAP=$TLRENDER_MMAP \
+    -DTLRENDER_PYTHON=$TLRENDER_PYTHON \
+    -DTLRENDER_GLFW=$TLRENDER_GLFW \
+    -DTLRENDER_OCIO=$TLRENDER_OCIO \
+    -DTLRENDER_AUDIO=$TLRENDER_AUDIO \
+    -DTLRENDER_JPEG=$TLRENDER_JPEG \
+    -DTLRENDER_TIFF=$TLRENDER_TIFF \
+    -DTLRENDER_STB=$TLRENDER_STB \
+    -DTLRENDER_PNG=$TLRENDER_PNG \
+    -DTLRENDER_EXR=$TLRENDER_EXR \
+    -DTLRENDER_FFMPEG=$TLRENDER_FFMPEG \
+    -DTLRENDER_QT5=$TLRENDER_QT5 \
+    -DTLRENDER_PROGRAMS=$TLRENDER_PROGRAMS \
+    -DTLRENDER_EXAMPLES=$TLRENDER_EXAMPLES \
+    -DTLRENDER_TESTS=$TLRENDER_TESTS \
+    -DTLRENDER_GCOV=$TLRENDER_GCOV
+cmake --build . -j 4 --config Debug
+
+# Run tests
+cd tlRender/src/tlRender-build
+ctest --rerun-failed --output-on-failure
