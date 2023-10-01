798aae02eae72266078cf174ba27a8181c657311
Darby Johnston <darbyjohnston@yahoo.com>
2023-10-01 14:19:53 -0700

    Exclude deps

--- etc/Linux/lcov.sh
+++ etc/Linux/lcov.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-lcov -c -b . -d . -o coverage.info
+lcov -c -b . -d . -o coverage.info --exclude deps
 lcov -r coverage.info '*/usr/*' -o coverage_filtered.info
 lcov -r coverage_filtered.info '*/install/*' -o coverage_filtered.info
 lcov -r coverage_filtered.info '*/tests/*' -o coverage_filtered.info
