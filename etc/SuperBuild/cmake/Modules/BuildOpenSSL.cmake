9fd217aecfa5d6b092b1df5f9749299e2cd17e72
Darby Johnston <darbyjohnston@yahoo.com>
2023-11-11 17:51:29 -0800

    Build OpenSSL static on macOS and Linux

--- etc/SuperBuild/cmake/Modules/BuildOpenSSL.cmake
+++ etc/SuperBuild/cmake/Modules/BuildOpenSSL.cmake
@@ -27,6 +27,9 @@ elseif(APPLE)
         no-external-tests
         no-tests
         no-unit-test)
+    if(NOT BUILD_SHARED_LIBS)
+        list(APPEND OpenSSL_CONFIGURE no-shared)
+    endif()
     if(CMAKE_OSX_DEPLOYMENT_TARGET)
         list(APPEND OpenSSL_CONFIGURE -mmacosx-version-min=${CMAKE_OSX_DEPLOYMENT_TARGET})
     endif()
@@ -40,6 +43,9 @@ else()
         no-external-tests
         no-tests
         no-unit-test)
+    if(NOT BUILD_SHARED_LIBS)
+        list(APPEND OpenSSL_CONFIGURE no-shared)
+    endif()
     set(OpenSSL_BUILD make)
     set(OpenSSL_INSTALL make install)
 endif()
