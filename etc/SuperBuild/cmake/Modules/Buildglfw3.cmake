d97fc957f942e4fa36a627e3ef55d1b498b084c3
Darby Johnston <darbyjohnston@yahoo.com>
2023-11-14 09:05:11 -0800

    Update GLFW version

--- etc/SuperBuild/cmake/Modules/Buildglfw3.cmake
+++ etc/SuperBuild/cmake/Modules/Buildglfw3.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 
 set(glfw3_GIT_REPOSITORY "https://github.com/glfw/glfw.git")
-set(glfw3_GIT_TAG "3.3.8")
+set(glfw3_GIT_TAG "3eaf1255b29fdf5c2895856c7be7d7185ef2b241")
 
 set(glfw3_ARGS
     ${TLRENDER_EXTERNAL_ARGS}
