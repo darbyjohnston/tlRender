8e61208b0b4420c155e88bf481cf729128daaf99
Darby Johnston <darbyjohnston@yahoo.com>
2023-12-30 11:25:41 -0800

    Package fix

--- cmake/Modules/Package.cmake
+++ cmake/Modules/Package.cmake
@@ -19,8 +19,8 @@ if(WIN32)
     
     if(TLRENDER_USD)
         set(BOOST_DLLS
-            ${CMAKE_INSTALL_PREFIX}/bin/boost_atomic-vc143-mt-x64-1_76.dll
-            ${CMAKE_INSTALL_PREFIX}/bin/boost_regex-vc143-mt-x64-1_76.dll)
+            ${CMAKE_INSTALL_PREFIX}/bin/boost_atomic-vc143-mt-x64-1_78.dll
+            ${CMAKE_INSTALL_PREFIX}/bin/boost_regex-vc143-mt-x64-1_78.dll)
         set(MATERIALX_DLLS
             ${CMAKE_INSTALL_PREFIX}/bin/MaterialXCore.dll
             ${CMAKE_INSTALL_PREFIX}/bin/MaterialXFormat.dll
@@ -299,9 +299,9 @@ else()
     if(TLRENDER_USD)
         set(BOOST_LIBS
             ${CMAKE_INSTALL_PREFIX}/lib/libboost_atomic.so
-            ${CMAKE_INSTALL_PREFIX}/lib/libboost_atomic.so.1.76.0
+            ${CMAKE_INSTALL_PREFIX}/lib/libboost_atomic.so.1.78.0
             ${CMAKE_INSTALL_PREFIX}/lib/libboost_regex.so
-            ${CMAKE_INSTALL_PREFIX}/lib/libboost_regex.so.1.76.0)
+            ${CMAKE_INSTALL_PREFIX}/lib/libboost_regex.so.1.78.0)
         set(MATERIALX_LIBS
             ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXCore.so
             ${CMAKE_INSTALL_PREFIX}/lib/libMaterialXCore.so.1
