fc5c6be437cf9d0d1b2b0bcfb079e7e15835c26c
Darby Johnston <darbyjohnston@yahoo.com>
2024-01-15 11:58:24 -0800

    Update copright dates

--- tests/tlCoreTest/FileInfoTest.cpp
+++ tests/tlCoreTest/FileInfoTest.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2021-2023 Darby Johnston
+// Copyright (c) 2021-2024 Darby Johnston
 // All rights reserved.
 
 #include <tlCoreTest/FileInfoTest.h>
--- tests/tltest/main.cpp
+++ tests/tltest/main.cpp
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BSD-3-Clause
-// Copyright (c) 2021-2023 Darby Johnston
+// Copyright (c) 2021-2024 Darby Johnston
 // All rights reserved.
 
 #if defined(TLRENDER_QT5) || defined(TLRENDER_QT6)
