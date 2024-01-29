2e192cbc299303dbb4eeb4b414043e71e82477d1
Darby Johnston <darbyjohnston@yahoo.com>
2024-01-29 10:49:47 -0800

    Add an option to disable the application delegate on macOS

--- etc/SuperBuild/cmake/Modules/Buildglfw3.cmake
+++ etc/SuperBuild/cmake/Modules/Buildglfw3.cmake
@@ -3,6 +3,14 @@ include(ExternalProject)
 set(glfw3_GIT_REPOSITORY "https://github.com/glfw/glfw.git")
 set(glfw3_GIT_TAG "3eaf1255b29fdf5c2895856c7be7d7185ef2b241")
 
+set(glfw3_PATCH)
+if(TLRENDER_GLFW_DISABLE_MACOS_APP_DELEGATE)
+    set(glfw3_PATCH
+        ${CMAKE_COMMAND} -E copy_if_different
+        ${CMAKE_CURRENT_SOURCE_DIR}/glfw3-patch/src/cocoa_init.m
+        ${CMAKE_CURRENT_BINARY_DIR}/glfw3/src/glfw3/src/cocoa_init.m)
+endif()
+
 set(glfw3_ARGS
     ${TLRENDER_EXTERNAL_ARGS}
     -DCMAKE_INSTALL_LIBDIR=lib
@@ -16,6 +24,7 @@ ExternalProject_Add(
     PREFIX ${CMAKE_CURRENT_BINARY_DIR}/glfw3
     GIT_REPOSITORY ${glfw3_GIT_REPOSITORY}
     GIT_TAG ${glfw3_GIT_TAG}
+    PATCH_COMMAND ${glfw3_PATCH}
     LIST_SEPARATOR |
     CMAKE_ARGS ${glfw3_ARGS})
 
