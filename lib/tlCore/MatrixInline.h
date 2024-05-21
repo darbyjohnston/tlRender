e9aaab473a8977d9d1474c725c91ca59092ebf0a
Darby Johnston <darbyjohnston@yahoo.com>
2024-05-21 15:47:05 -0700

    Remove extra constructors

--- lib/tlCore/MatrixInline.h
+++ lib/tlCore/MatrixInline.h
@@ -242,24 +242,21 @@ namespace tl
         template<typename T>
         inline Vector3<T> operator * (const Matrix4x4<T>& a, const Vector3<T>& v)
         {
-            Vector3<T> out;
-            T x = T (v[0] * a.e[0] + v[1] * a.e[4] + v[2] * a.e[8]  + a.e[12]);
-            T y = T (v[0] * a.e[1] + v[1] * a.e[5] + v[2] * a.e[9]  + a.e[13]);
-            T z = T (v[0] * a.e[2] + v[1] * a.e[6] + v[2] * a.e[10] + a.e[14]);
-            T w = T (v[0] * a.e[3] + v[1] * a.e[7] + v[2] * a.e[11] + a.e[15]);
-
-            out = Vector3<T> (x / w, y / w, z / w);
-            return out;
+            const T x = v[0] * a.e[0] + v[1] * a.e[4] + v[2] * a.e[8]  + a.e[12];
+            const T y = v[0] * a.e[1] + v[1] * a.e[5] + v[2] * a.e[9]  + a.e[13];
+            const T z = v[0] * a.e[2] + v[1] * a.e[6] + v[2] * a.e[10] + a.e[14];
+            const T w = v[0] * a.e[3] + v[1] * a.e[7] + v[2] * a.e[11] + a.e[15];
+            return Vector3<T>(x / w, y / w, z / w);
         }
 
         template<typename T>
         inline Vector4<T> operator * (const Matrix4x4<T>& a, const Vector4<T>& v)
         {
             Vector4<T> out;
-            out.x = T (v[0] * a.e[0] + v[1] * a.e[4] + v[2] * a.e[8]  + v[3] * a.e[12]);
-            out.y = T (v[0] * a.e[1] + v[1] * a.e[5] + v[2] * a.e[9]  + v[3] * a.e[13]);
-            out.z = T (v[0] * a.e[2] + v[1] * a.e[6] + v[2] * a.e[10] + v[3] * a.e[14]);
-            out.w = T (v[0] * a.e[3] + v[1] * a.e[7] + v[2] * a.e[11] + v[3] * a.e[15]);
+            out.x = v[0] * a.e[0] + v[1] * a.e[4] + v[2] * a.e[8]  + v[3] * a.e[12];
+            out.y = v[0] * a.e[1] + v[1] * a.e[5] + v[2] * a.e[9]  + v[3] * a.e[13];
+            out.z = v[0] * a.e[2] + v[1] * a.e[6] + v[2] * a.e[10] + v[3] * a.e[14];
+            out.w = v[0] * a.e[3] + v[1] * a.e[7] + v[2] * a.e[11] + v[3] * a.e[15];
             return out;
         }
     };
