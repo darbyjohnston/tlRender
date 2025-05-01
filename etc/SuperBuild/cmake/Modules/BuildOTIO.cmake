6b8c276081be75bfc98e683ec6a642f7d8f41cd6
Darby Johnston <darbyjohnston@yahoo.com>
2025-05-01 09:44:11 -0700

    Add OTIO patch

--- etc/SuperBuild/cmake/Modules/BuildOTIO.cmake
+++ etc/SuperBuild/cmake/Modules/BuildOTIO.cmake
@@ -3,6 +3,10 @@ include(ExternalProject)
 set(OTIO_GIT_REPOSITORY "https://github.com/PixarAnimationStudios/OpenTimelineIO.git")
 set(OTIO_GIT_TAG "v0.17.0")
 
+set(OTIO_PATCH ${CMAKE_COMMAND} -E copy_if_different
+    ${CMAKE_CURRENT_SOURCE_DIR}/OTIO-patch/src/opentimelineio/CMakeLists.txt
+    ${CMAKE_CURRENT_BINARY_DIR}/OTIO/src/OTIO/src/opentimelineio/CMakeLists.txt)
+
 set(OTIO_SHARED_LIBS ON)
 if(NOT BUILD_SHARED_LIBS)
     set(OTIO_SHARED_LIBS OFF)
@@ -20,5 +24,6 @@ ExternalProject_Add(
     DEPENDS Imath
     GIT_REPOSITORY ${OTIO_GIT_REPOSITORY}
     GIT_TAG ${OTIO_GIT_TAG}
+    PATCH_COMMAND  ${OTIO_PATCH}
     LIST_SEPARATOR |
     CMAKE_ARGS ${OTIO_ARGS})
