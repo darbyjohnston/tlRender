ae846ade02ecd3c2f61ea74d00861576359c5b03
Darby Johnston <darbyjohnston@yahoo.com>
2025-05-05 12:27:46 -0700

    Build fix

--- etc/SuperBuild/cmake/Modules/BuildZLIB.cmake
+++ etc/SuperBuild/cmake/Modules/BuildZLIB.cmake
@@ -6,11 +6,11 @@ set(ZLIB_GIT_TAG "v1.3.1")
 set(ZLIB_PATCH ${CMAKE_COMMAND} -E copy_if_different
     ${CMAKE_CURRENT_SOURCE_DIR}/ZLIB-patch/CMakeLists.txt
     ${CMAKE_CURRENT_BINARY_DIR}/ZLIB/src/ZLIB/CMakeLists.txt)
-#if(WIN32)
-#    list(APPEND ZLIB_PATCH COMMAND ${CMAKE_COMMAND} -E copy_if_different
-#        ${CMAKE_CURRENT_SOURCE_DIR}/ZLIB-patch/zconf.h.cmakein
-#        ${CMAKE_CURRENT_BINARY_DIR}/ZLIB/src/ZLIB/zconf.h.cmakein)
-#endif()
+if(WIN32)
+    list(APPEND ZLIB_PATCH COMMAND ${CMAKE_COMMAND} -E copy_if_different
+        ${CMAKE_CURRENT_SOURCE_DIR}/ZLIB-patch/zconf.h.cmakein
+       ${CMAKE_CURRENT_BINARY_DIR}/ZLIB/src/ZLIB/zconf.h.cmakein)
+endif()
 
 set(ZLIB_ARGS
     ${TLRENDER_EXTERNAL_ARGS}
