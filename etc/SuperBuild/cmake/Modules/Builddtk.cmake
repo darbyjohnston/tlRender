2fbabad46c3bb674be233a2b0e0d1122a78009e5
Darby Johnston <darbyjohnston@yahoo.com>
2025-06-20 17:57:26 -0700

    Update dtk

--- etc/SuperBuild/cmake/Modules/Builddtk.cmake
+++ etc/SuperBuild/cmake/Modules/Builddtk.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 
 set(dtk_GIT_REPOSITORY "https://github.com/darbyjohnston/dtk.git")
-set(dtk_GIT_TAG "c8aed0494bfdde7ae7dab4288f72b57c78cdb904")
+set(dtk_GIT_TAG "87d8b24c097265f1b1b352033f59bfe7f6bfa3b6")
 
 set(dtk_DEPS ZLIB)
 set(dtk_ARGS
