9abfe119b94dee606b35ca567e739734e0d7ddbe
Darby Johnston <darbyjohnston@yahoo.com>
2025-08-18 17:42:07 -0700

    Update feather-tk

--- etc/SuperBuild/cmake/Modules/Buildfeather-tk.cmake
+++ etc/SuperBuild/cmake/Modules/Buildfeather-tk.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 
 set(feather_tk_GIT_REPOSITORY "https://github.com/darbyjohnston/feather-tk.git")
-set(feather_tk_GIT_TAG "b227769cc9e627ecbbbe4e9e160cd729ddb7edf9")
+set(feather_tk_GIT_TAG "b4664c4b3b904ebdb676b2e370c57fac78222563")
 
 set(feather_tk_DEPS ZLIB PNG)
 set(feather_tk_ARGS
