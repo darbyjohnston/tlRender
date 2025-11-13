cc465ddff35b3cf4df187240b27ff8ef756a4e5d
Darby Johnston <darbyjohnston@yahoo.com>
2025-11-12 18:05:56 -0800

    Downgrade OpenEXR version

--- etc/SuperBuild/cmake/Modules/BuildOpenEXR.cmake
+++ etc/SuperBuild/cmake/Modules/BuildOpenEXR.cmake
@@ -1,7 +1,7 @@
 include(ExternalProject)
 
 set(OpenEXR_GIT_REPOSITORY "https://github.com/AcademySoftwareFoundation/openexr.git")
-set(OpenEXR_GIT_TAG "v3.4.3")
+set(OpenEXR_GIT_TAG "v3.3.6")
 
 set(OpenEXR_ARGS
     ${TLRENDER_EXTERNAL_ARGS}
